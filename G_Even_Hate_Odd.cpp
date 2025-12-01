c45327cde7b14a89affea568f9d4c443bfaf4827
ArfinAlEusuf <arfinaleusuf@gmail.com>
2025-12-01 16:11:06 +0600

    even hate odd complited

--- G_Even_Hate_Odd.cpp
+++ G_Even_Hate_Odd.cpp
@@ -8,13 +8,13 @@ int main()
     {
         int n;
         cin >> n;
+        int a[n];
+        for (int j = 0; j < n; j++)
+        {
+            cin >> a[j];
+        }
         if (n % 2 == 0)
         {
-            int a[n];
-            for (int j = 0; j < n; j++)
-            {
-                cin >> a[j];
-            }
             int mid = n / 2;
             int even = 0;
             for (int j = 0; j < n; j++)
@@ -33,35 +33,3 @@ int main()
     }
     return 0;
 }
-//#include <bits/stdc++.h>
-// using namespace std;
-
-// int main() {
-//     ios::sync_with_stdio(false);
-//     cin.tie(NULL);
-
-//     int t;
-//     cin >> t;
-//     while(t--) {
-//         int n;
-//         cin >> n;
-
-//         vector<int> a(n);
-//         int cnt_even = 0;
-
-//         for(int i = 0; i < n; i++) {
-//             cin >> a[i];
-//             if(a[i] % 2 == 0) cnt_even++;
-//         }
-
-//         if(n % 2 == 1) {
-//             cout << -1 << "\n";
-//             continue;
-//         }
-
-//         int target = n / 2;
-//         cout << abs(cnt_even - target) << "\n";
-//     }
-
-//     return 0;
-// }
