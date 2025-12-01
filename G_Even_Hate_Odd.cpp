e6bcd3204922e77b1241e1a941751daa9a5ae0e7
ArfinAlEusuf <arfinaleusuf@gmail.com>
2025-12-01 15:07:00 +0600

    practice 1 complite

--- G_Even_Hate_Odd.cpp
+++ G_Even_Hate_Odd.cpp
@@ -10,23 +10,18 @@ int main()
         cin >> n;
         if (n % 2 == 0)
         {
-            vector<int> a(n);
+            int a[n];
             for (int j = 0; j < n; j++)
             {
                 cin >> a[j];
             }
             int mid = n / 2;
             int even = 0;
-            int odd = 0;
             for (int j = 0; j < n; j++)
             {
                 if (a[j] % 2 == 0)
                 {
                     even++;
-                }
-                else
-                {
-                    odd++;
                 }
             }
             cout << abs(even - mid) << endl;
@@ -38,3 +33,35 @@ int main()
     }
     return 0;
 }
+//#include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+//     ios::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     int t;
+//     cin >> t;
+//     while(t--) {
+//         int n;
+//         cin >> n;
+
+//         vector<int> a(n);
+//         int cnt_even = 0;
+
+//         for(int i = 0; i < n; i++) {
+//             cin >> a[i];
+//             if(a[i] % 2 == 0) cnt_even++;
+//         }
+
+//         if(n % 2 == 1) {
+//             cout << -1 << "\n";
+//             continue;
+//         }
+
+//         int target = n / 2;
+//         cout << abs(cnt_even - target) << "\n";
+//     }
+
+//     return 0;
+// }
